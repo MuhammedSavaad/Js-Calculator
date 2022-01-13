feb8f09462b3208487896e9d3ab6b5f6f1a059b8
savadMKD <savadmoorkanad@gmail.com>
2022-01-13 19:27:52 +0530

    Pop Up Designed

---
+++ script.js
@@ -0,0 +1,12 @@
+var popup = document.getElementById("popup");
+
+let formValidated = localStorage.getItem("FormValidated");
+console.log(formValidated);
+
+function FormValidation(){
+    localStorage.setItem("FormValidated", true)
+}
+
+if(localStorage.getItem("FormValidated")=="true"){
+    popup.classList.add("hide");
+}
