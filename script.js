1cea64dd81befa3d9fb17a90a4b479ca9ef58e15
savadMKD <savadmoorkanad@gmail.com>
2022-01-13 22:15:18 +0530

    All Designs Are Completed....

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var popup = document.getElementById("popup");
+var body = document.body;
 
 let formValidated = localStorage.getItem("FormValidated");
 console.log(formValidated);
@@ -7,8 +8,14 @@ function FormValidation(){
     localStorage.setItem("FormValidated", true)
 }
 
+if(localStorage.getItem("FormValidated")==null){
+    body.classList.add("hide");
+}
+
 if(localStorage.getItem("FormValidated")=="true"){
+    body.classList.remove("hide");
     popup.classList.add("hide");
 }
+
 
 // localStorage.removeItem("FormValidated");
